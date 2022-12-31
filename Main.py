81226ad4d3ff23efacd068ecd40832708158ed21
Pranay <satputepranay01@gmail.com>
2022-12-31 18:20:01 +0530

    Added a Program to add two numbers

--- Main.py
+++ Main.py
@@ -7,6 +7,18 @@ def animal():
     pass
 def Printing_message():
     print("I am Pranay Satpute")
+    
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
 
 
 
